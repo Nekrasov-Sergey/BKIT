5c51c29bf5fd332493d17beca7436ecf10ce41b6
Сергей <90382754+Nekrasov-Sergey@users.noreply.github.com>
2021-12-14 20:06:45 +0300

    Add files via upload

---
+++ Lab6/app/handlers/ticket.py
@@ -0,0 +1,67 @@
+from aiogram import Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+available_city_name = ["Анапа", "Сочи", "Ялта"]
+available_day_quantity = ["5", "7", "9"]
+available_trip_method = ["самолёт", "поезд", "яхта"]
+
+
+class OrderTicket(StatesGroup):
+    waiting_for_city_name = State()
+    waiting_for_day_quantity = State()
+    waiting_for_trip_method = State()
+
+
+async def ticket_start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for name in available_city_name:
+        keyboard.add(name)
+    await message.answer("Выберите город:", reply_markup=keyboard)
+    await OrderTicket.waiting_for_city_name.set()
+
+
+
+async def ticket_chosen(message: types.Message, state: FSMContext):
+    if message.text not in available_city_name:
+        await message.answer("В данный город нет билетов, выберите город, используя клавиатуру ниже")
+        return
+    await state.update_data(chosen_ticket=message.text)
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for quantity in available_day_quantity:
+        keyboard.add(quantity)
+    await OrderTicket.next()
+    await message.answer("Теперь выберите количество дней:", reply_markup=keyboard)
+
+
+async def ticket_quantity_chosen(message: types.Message, state: FSMContext):
+    if message.text not in available_day_quantity:
+        await message.answer(
+            "На такое количество дней нет билетов, выберите количество дней, используя клавиатуру ниже")
+        return
+    await state.update_data(chosen_days=message.text)
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for method in available_trip_method:
+        keyboard.add(method)
+    await OrderTicket.next()
+    await message.answer("Теперь выберите способ поездки:", reply_markup=keyboard)
+
+
+async def ticket_method_chosen(message: types.Message, state: FSMContext):
+    if message.text not in available_trip_method:
+        await message.answer("Такого способа поездки нет, выберите способ поездки, используя клавиатуру ниже")
+        return
+    user_data = await state.get_data()
+    await message.answer(f"Вы отдохнёте {user_data['chosen_days']} дней в городе {user_data['chosen_ticket']}. "
+                         f"Ваш способ поездки: {message.text}\n",
+                         reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
+
+
+def register_handlers_ticket(dp: Dispatcher):
+    dp.register_message_handler(ticket_start, commands="ticket", state="*")
+    dp.register_message_handler(ticket_chosen, state=OrderTicket.waiting_for_city_name)
+    dp.register_message_handler(ticket_quantity_chosen, state=OrderTicket.waiting_for_day_quantity)
+    dp.register_message_handler(ticket_method_chosen, state=OrderTicket.waiting_for_trip_method)
